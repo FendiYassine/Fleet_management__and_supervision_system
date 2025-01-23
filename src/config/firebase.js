f0f3654a8a818238f417991c737a6df6a9b9b876
Yassine Fendi <132173035+FendiYassine@users.noreply.github.com>
2025-01-23 11:06:47 +0100

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBCw06q_uCE7oheJEquse49sClINCW1-lI',
-  authDomain: 'fir-test-364d7.firebaseapp.com',
-  databaseURL: 'https://fir-test-364d7-default-rtdb.firebaseio.com',
-  projectId: 'fir-test-364d7',
-  storageBucket: 'fir-test-364d7.appspot.com',
-  messagingSenderId: '842960228253',
-  appId: '1:842960228253:web:88e9f47662f633f14f2974',
+  apiKey: '__change this',
+  authDomain: '__change this',
+  databaseURL: '__change this',
+  projectId: '__change this',
+  storageBucket: '__change this',
+  messagingSenderId: '__change this',
+  appId: '__change this',
 };
 
 const app = initializeApp(firebaseConfig);
